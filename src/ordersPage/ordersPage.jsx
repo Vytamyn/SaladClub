cf961a5cfca61aa46356b63accbf6e067e94e0a9
Vytamyn <141726927+Vytamyn@users.noreply.github.com>
2023-12-22 13:40:35 +0100

    Safe save

---
+++ src/ordersPage/ordersPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ordersPage.css";
+
+function OrdersPage() {
+  return (
+    <div>
+      <h2>Orders Page</h2>
+    </div>
+  );
+}
+
+export default OrdersPage;
