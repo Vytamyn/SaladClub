99eab0e110a8bf25f544c3c8bb20be07c740784a
Vytamyn <141726927+Vytamyn@users.noreply.github.com>
2024-01-04 11:49:01 +0100

    Orders page done

--- src/ordersPage/ordersPage.jsx
+++ src/ordersPage/ordersPage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./ordersPage.css";
+import data from "../assets/data";
 import Sidebar from "../assets/sidebar";
 import SearchBar from "../assets/searchBar";
 import "../assets/searchBar.css";
 import ongoingIcon from "../img/ongoingIcon.png";
 import deliveredIcon from "../img/deliveredIcon.png";
 import ordersIcon from "../img/ordersIcon.png";
-import data from "../assets/data";
 import upChartIcon from "../img/upChartIcon.png";
 import downChartIcon from "../img/downChartIcon.png";
 
@@ -18,6 +18,8 @@ function OrdersPage() {
     (order) => order.Status === "Delivered"
   ).length;
   const totalOrdersCount = data.length;
+
+  const displayedData = data;
 
   return (
     <div className="main-frame">
@@ -71,7 +73,48 @@ function OrdersPage() {
           <div className="orders-page-table-header">
             <h2>Orders</h2>
           </div>
-          <div className="orders-page-table">table</div>
+          <div className="orders-page-table">
+            <table>
+              <thead>
+                <tr className="orders-table-header">
+                  <th className="orders-header-restaurant">Restaurant</th>
+                  <th className="orders-header-order">Order</th>
+                  <th className="orders-header-order-id">Order ID</th>
+                  <th className="orders-header-client-name">Client Name</th>
+                  <th className="orders-header-delivery-address">
+                    Delivery Address
+                  </th>
+                  <th className="orders-header-value">Value</th>
+                  <th className="orders-header-eta">ETA</th>
+                  <th className="orders-header-driver">Driver</th>
+                  <th className="orders-header-status">Staturs</th>
+                </tr>
+              </thead>
+              <tbody>
+                {displayedData.map((order, index) => (
+                  <tr key={index} className="table-row">
+                    <td className="orders-table-restaurant">
+                      {order.Restaurant}
+                    </td>
+                    <td className="orders-table-order">
+                      {order.Order.join(", ")}
+                    </td>
+                    <td className="orders-table-order-id">{order.OrderId}</td>
+                    <td className="orders-table-client-name">
+                      {order.ClientName}
+                    </td>
+                    <td className="orders-table-delivery-addres">
+                      {order.DeliveryAddress}
+                    </td>
+                    <td className="orders-table-value">{order.Value}</td>
+                    <td className="orders-table-eta">{order.ETA}</td>
+                    <td className="orders-table-driver">{order.Driver}</td>
+                    <td className="orders-table-status">{order.Status}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
