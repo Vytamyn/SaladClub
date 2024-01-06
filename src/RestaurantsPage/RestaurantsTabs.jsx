06d6a6dd2f01e5b2385018c9b8b7e295f5b5636a
Vytamyn <141726927+Vytamyn@users.noreply.github.com>
2024-01-06 12:29:56 +0100

    Map save

--- src/RestaurantsPage/RestaurantsTabs.jsx
+++ src/RestaurantsPage/RestaurantsTabs.jsx
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import restaurantsData from "../assets/restaurantsData";
 import manageIcon from "../img/manage icon.png";
 import phoneIcon from "../img/phone icon.png";
 import mailIcon from "../img/mail icon.png";
 
 function RestaurantsTabs() {
+  const [restaurants, setRestaurants] = useState(restaurantsData);
+
+  const handleManageClick = (index) => {
+    const updatedRestaurants = [...restaurants];
+    updatedRestaurants[index].isActive = !updatedRestaurants[index].isActive;
+    setRestaurants(updatedRestaurants);
+  };
+
   return (
     <div className="restaurant-tabs-container">
-      {restaurantsData.map((restaurant, index) => (
-        <div className="restaurants-tabs" key={index}>
+      {restaurants.map((restaurant, index) => (
+        <div className="restaurants-tabs" key={restaurant.id}>
           <div className="restaurants-tab-image">
             <img src={restaurant.Image} alt={`Image of ${restaurant.Name}`} />
           </div>
           <div className="restaurants-tab-info">
-            <h3>{restaurant.Name}</h3>
+            <div className="restaurants-tab-header">
+              <h3>{restaurant.Name}</h3>
+              <img
+                src={
+                  restaurant.isActive
+                    ? restaurant.ActiveIcon
+                    : restaurant.InactiveIcon
+                }
+              />
+            </div>
             <h3>{restaurant.Location}</h3>
             <div className="restaurants-tab-info-open">
               <p className="p-gray">{restaurant.Open}</p>
@@ -22,6 +39,7 @@ function RestaurantsTabs() {
               <button
                 className="button-black-bordered"
                 style={{ marginRight: "8px" }}
+                onClick={() => handleManageClick(index)}
               >
                 <img
                   src={manageIcon}
--- src/assets/restaurantsData.js
+++ src/assets/restaurantsData.js
@@ -2,31 +2,49 @@ import restaurantOne from "../img/restaurant1.png";
 import restaurantTwo from "../img/restaurant2.png";
 import restaurantThree from "../img/restaurant3.png";
 import restaurantFour from "../img/restaurant4.png";
+import restaurantActiveIcon from "../img/restaurant icon active.png";
+import restaurantInactiveIcon from "../img/restaurant icon inactive.png";
 
 const restaurantsData = [
   {
+    id: 1,
     Name: "SaladClub",
-    Location: "Košice SK, Hlavná 102, Staré Mesto",
+    Location: "Hlavná 102, Košice",
     Open: "10:00am - 9:00pm, Mon-Sat",
     Image: restaurantOne,
+    ActiveIcon: restaurantActiveIcon,
+    InactiveIcon: restaurantInactiveIcon,
+    isActive: true,
   },
   {
+    id: 2,
     Name: "SaladClub",
     Location: "Hlavná 37/68, Košice",
     Open: "10:00am - 9:00pm, Mon-Sat",
     Image: restaurantTwo,
+    ActiveIcon: restaurantActiveIcon,
+    InactiveIcon: restaurantInactiveIcon,
+    isActive: true,
   },
   {
+    id: 3,
     Name: "SaladClub",
     Location: "Masarykova 1667/2, Košice",
     Open: "10:00am - 9:00pm, Mon-Sat",
     Image: restaurantThree,
+    ActiveIcon: restaurantActiveIcon,
+    InactiveIcon: restaurantInactiveIcon,
+    isActive: true,
   },
   {
+    id: 4,
     Name: "SaladClub",
     Location: "Murgašova 1, Košice",
     Open: "10:00am - 9:00pm, Mon-Sat",
     Image: restaurantFour,
+    ActiveIcon: restaurantActiveIcon,
+    InactiveIcon: restaurantInactiveIcon,
+    isActive: true,
   },
 ];
 
